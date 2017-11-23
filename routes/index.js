21003b86644856a407cf4318961015749be893e1
王伟峰 <980090631@qq.com>
2017-11-23 17:30:44 +0800

    <增加><增加了爱奇艺的登陆><*>

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const puppeteer = require('puppeteer')
+puppeteer.launch({headless: false}).then(async browser => {
+  const page = await browser.newPage()
+  //page.setViewport({width: 1200, height: 600})
+ 
+  try {
+      await page.goto('http://www.iqiyi.com/iframe/loginreg?is_reg=1&');
+      await page.waitFor(1000);
+      await page.type('input[data-regbox="name"]', "15555373875", { delay: 100 });
+      await page.click('[rseat="prgd_smsbtn"]');
+  } catch (e) {
+      console.log("e:"+e);
+  }
+
+ 
+  // await page.close()
+  // browser.close()
+})
 
 
 
